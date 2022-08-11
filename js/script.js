a11419f8f3736950203564d7abbdd3bd434421cf
Tolulope Soetan <Tolulopesoetan1@gmail.com>
2022-08-11 20:28:53 +0100

    First commit

---
+++ js/script.js
@@ -0,0 +1,55 @@
+const btn = document.getElementById("menu-btn");
+const overlay = document.getElementById("overlay");
+const menu = document.getElementById("mobile-menu");
+const counters = document.querySelectorAll(".counter");
+let scrollStarted = false;
+
+const navToggle = () => {
+    btn.classList.toggle("open")
+    overlay.classList.toggle("overlay-show")
+    document.body.classList.toggle("stop-scrolling")
+    menu.classList.toggle("show-menu")
+}
+
+const countUp = () => {
+    counters.forEach((counter) => {
+        counter.innerText = "0"
+
+        const updateCounter = () => {
+            const target = +counter.getAttribute("data-target")
+
+            const counterValue = +counter.innerText;
+
+            if(counterValue < target){
+                counter.innerText = `${counterValue + 1}`
+            } else{
+                counter.innerText = target
+            }
+
+            setTimeout(updateCounter, 100)
+        }
+
+        updateCounter()
+    })
+}
+
+const reset = () => {
+    counters.forEach((counter) => {
+        counter.innerHTML = "0"
+    })
+}
+
+scrollPage = () => {
+    const scrollPos = window.scrollY
+
+    if(scrollPos > 80 && !scrollStarted){
+        countUp()
+        scrollStarted = true
+    } else if(scrollPos < 80 && scrollStarted){
+        reset()
+        scrollStarted = false
+    }
+}
+
+btn.addEventListener("click", navToggle)
+document.addEventListener("scroll", scrollPage)
